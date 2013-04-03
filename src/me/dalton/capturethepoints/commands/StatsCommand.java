bd7323025380696788e274bde8c61cc29c506c6f
graywolf336 <graywolf336@craftyn.com>
2013-04-03 00:21:17 -0500

    Fix an NPE on the stats command

--- src/me/dalton/capturethepoints/commands/StatsCommand.java
+++ src/me/dalton/capturethepoints/commands/StatsCommand.java
@@ -31,7 +31,7 @@ public class StatsCommand extends CTPCommand {
         
         Arena a = ctp.getArenaMaster().getArenaPlayerIsIn(player.getName());
         PlayerData pdata = a.getPlayerData(player.getName());
-        ChatColor cc = pdata.getTeam().getChatColor(), white = ChatColor.WHITE, green = ChatColor.GREEN;
+        ChatColor cc = pdata.getTeam().getChatColor() == null ? ChatColor.WHITE : pdata.getTeam().getChatColor(), white = ChatColor.WHITE, green = ChatColor.GREEN;
         
         sendMessage(cc + "Your Stats: ");
         sendMessage(cc + "  Kills: " + white + pdata.getKills() + " (Streak: " + pdata.getKillsInARow() + ")");
