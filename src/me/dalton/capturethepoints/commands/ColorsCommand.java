7427ee9778ca1bafaa21d68634f52ddf6cffaed0
graywolf336 <graywolf336@craftyn.com>
2013-08-17 00:46:40 -0500

    If the parameters for colors is two, just show usage or if admin show colors

--- src/me/dalton/capturethepoints/commands/ColorsCommand.java
+++ src/me/dalton/capturethepoints/commands/ColorsCommand.java
@@ -18,18 +18,24 @@ public class ColorsCommand extends CTPCommand {
         super.notOpCommand = true;
         super.requiredPermissions = new String[]{"ctp.*", "ctp.play", "ctp.admin", "ctp.colors"};
         super.senderMustBePlayer = true;
-        super.minParameters = 3;
+        super.minParameters = 2;
         super.maxParameters = 3;
         super.usageTemplate = "/ctp colors <arena>";
     }
 
     @Override
-    public void perform() {       
-        if (ctp.getPermissions().canAccess(player, false, new String[]{"ctp.*", "ctp.admin"})) {
-            sendMessage(ChatColor.RED + "Admin: " + ChatColor.BLUE + "Available team colors:"); // Kj -- typo ;)
-            sendMessage(ChatColor.GREEN + "WHITE, LIGHTGRAY, GRAY, BLACK, RED, ORANGE, YELLOW, LIME, LIGHTBLUE, GREEN, CYAN, BLUE, PURPLE, MAGENTA, PINK, BROWN");
-        }
-        
+    public void perform() {
+    	if(parameters.size() == 2) {
+    		if (ctp.getPermissions().canAccess(player, false, new String[]{"ctp.*", "ctp.admin"})) {
+                sendMessage(ChatColor.RED + "Admin: " + ChatColor.BLUE + "Available team colors:"); // Kj -- typo ;)
+                sendMessage(ChatColor.GREEN + "WHITE, LIGHTGRAY, GRAY, BLACK, RED, ORANGE, YELLOW, LIME, LIGHTBLUE, GREEN, CYAN, BLUE, PURPLE, MAGENTA, PINK, BROWN");
+                return; //if only colors, then just send colors
+            }else {
+            	usageError();
+            	return;
+            }
+    	}
+
         if(ctp.getArenaMaster().getArenas().isEmpty()) {
         	sendMessage(ctp.getLanguage().checks_NO_ARENAS);
         	return;
